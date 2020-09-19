16216a14a0643bdb74495d72735c0d12be2b07ff
pabloh1995 <50746699+pabloh1995@users.noreply.github.com>
2020-09-19 19:09:06 -0300

    Add files via upload

---
+++ ElDeporteOnline/src/test/java/com/unla/elDeporteOnline/ElDeporteOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unla.elDeporteOnline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElDeporteOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
